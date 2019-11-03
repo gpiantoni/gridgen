dd2e0113bc41b6a579949e84e5f4cd658fe4d243
Gio on ArchXPS <github@gpiantoni.com>
2019-11-03 19:51:56 +0100

    remove compute neighbor

--- gridloc/geometry.py
+++ gridloc/geometry.py
@@ -1,4 +1,4 @@
-from numpy import ones, array, zeros, argmin
+from numpy import array, zeros, argmin
 from numpy.linalg import norm
 from gridloc.algebra import calc_plane_to_axis
 
@@ -23,6 +23,10 @@ def count_neighbors(grid, x, y):
 
     n = idx.sum()
 
+    """make sure that any indexing does not have more than 2 neighbors
+    Also, when you have two neighbors, the order is important. If you draw a
+    line from the first neighbor to the second neighbor looking from above,
+    the target position should be on the right"""
     if (idx == array([True, False, False, True])).all():
         coords = NEIGHBORS[(3, 0), :] + (x, y)
     else:
@@ -40,52 +44,3 @@ def search_grid(surf, ref_vert, x, y):
     new_vector = argmin(norm(surf['pos'] - target, axis=1))
 
     return new_vector
-
-
-def compute_neighbor(n_rows, n_cols, index='up_down'):
-    """
-    Deprecated
-
-    make sure that any indexing does not have more than 2 neighbors
-    Also, when you have two neighbors, the order is important. If you draw a
-    line from the first neighbor to the second neighbor looking from above,
-    the target position should be on the right
-    """
-    # include padding, to avoid indexerror
-    neigh = -1 * ones((n_rows + 2, n_cols + 2, 2, 2), dtype=int)
-
-    if index == 'up_down':
-        g = index_up_down(n_rows, n_cols)
-    elif index.startswith('corner'):
-        g = index_corner(n_rows, n_cols, start=index[7:])
-    else:
-        raise NotImplementedError(f'index {index} not implemented')
-
-    [x_start, y_start] = next(g)
-    neigh[x_start + 1, y_start + 1, 0, :] = [x_start, y_start]
-
-    neighbors = array([
-        [-1, 0],
-        [0, 1],
-        [1, 0],
-        [0, -1],
-        ])
-
-    for x, y in g:
-        n = neighbors + (x, y)
-        idx = (neigh[n[:, 0] + 1, n[:, 1] + 1, 0, 0] != -1)
-        if idx.sum() == 1:
-            neigh[x + 1, y + 1, 0, :] = n[idx]
-        elif idx.sum() == 2:
-            if (idx == array([True, False, False, True])).all():
-                neigh[x + 1, y + 1, 0, :] = n[3]
-                neigh[x + 1, y + 1, 1, :] = n[0]
-            else:
-                neigh[x + 1, y + 1, :, :] = n[idx]
-        else:
-            raise IndexError('There is something wrong')
-
-    neigh[x_start + 1, y_start + 1, 0, :] = -1
-    neigh = neigh[1:-1, 1:-1, :, :]  # remove padding
-
-    return neigh
